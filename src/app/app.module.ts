84b967d4984d67cf450f1c5416d615eb2aef04ac
Filip de Tillier <filipdetillier@gmail.com>
2019-07-12 20:30:50 +0200

    simple 3 list, drag and drop without api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BoardComponent } from './components/board/board.component';
-import { DragDropModule } from '@angular/cdk/drag-drop';
 
 @NgModule({
   declarations: [
--- src/app/components/board/board.component.ts
+++ src/app/components/board/board.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+import { ViewportScroller } from '@angular/common';
 
 @Component({
   selector: 'app-board',
@@ -7,10 +8,38 @@ import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/dr
   styleUrls: ['./board.component.scss']
 })
 export class BoardComponent implements OnInit {
+  public todo = [
+    'task1',
+    'task2',
+    'task3',
+  ];
+  public doing = [
+    'task4',
+    'task5',
+    'task6',
 
-  constructor() { }
+  ];
+  public done = [
+    'task7',
+    'task8',
+    'task9',
+
+  ];
+
+  constructor(private ViewportScroller: ViewportScroller) {
+    this.ViewportScroller;
+  }
 
   ngOnInit() {
+  }
+
+  drop(event: CdkDragDrop<string[]>) {
+    const { previousContainer, container, previousIndex, currentIndex } = event;
+    if (previousContainer === container) {
+      moveItemInArray(container.data, previousIndex, currentIndex);
+    } else {
+      transferArrayItem(previousContainer.data, container.data, previousIndex, currentIndex);
+    }
   }
 
 }
