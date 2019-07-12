bb9e457c909fea054ab28c9fdab5326fe0530c37
Filip de Tillier <filipdetillier@gmail.com>
2019-07-12 18:28:32 +0200

    init

---
+++ src/app/components/board/board.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+
+@Component({
+  selector: 'app-board',
+  templateUrl: './board.component.html',
+  styleUrls: ['./board.component.scss']
+})
+export class BoardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
